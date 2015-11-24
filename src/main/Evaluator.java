8b315e13516fe1ca3d8fcb83c361502c32191ac2
bxhu <bxhu@live.unc.edu>
2015-11-23 23:32:41 -0500

    Added Histogram Calculator

---
+++ src/main/Evaluator.java
@@ -0,0 +1,47 @@
+package main;
+
+import java.awt.*;
+import java.awt.image.*;
+
+public class Evaluator {
+	
+	static int[][][] RGB = new int[8][8][8];
+	static int width, height;
+	
+	public static int[][][] sortImage(BufferedImage image) {
+		width = image.getWidth();
+		height = image.getHeight();
+		int pixel;
+		Color c;
+		
+		int red;
+		int green;
+		int blue;
+		
+		for (int i=0; i<width; i++) {
+		for (int j=0; j<height; j++) {
+			pixel = image.getRGB(i, j);
+			c = new Color(pixel);
+			
+			RGB[c.getRed()/32][c.getGreen()/32][c.getBlue()/32] += 1;
+		}
+		}
+		
+		return RGB;
+	}
+	
+	public static void printHistogram(int[][][] colors) {
+		String s = "";
+		
+		for (int x=0; x<8; x++) {
+		for (int y=0; y<8; y++) {
+		for (int z=0; z<8; z++) {
+			s += colors[x][y][z] + " ";
+		}
+			System.out.println(s);
+			s = "";
+		}
+			System.out.println("-----------------");
+		}
+	}
+}
--- src/main/main.java
+++ src/main/main.java
@@ -1,14 +1,22 @@
 package main;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
 public class main {
 
 	public main() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// test
-
+		BufferedImage image;
+		image = ImageHelper.readImage("images/clutch/img_bags_clutch_10.jpg");
+		
+		int[][][] test;
+		test = Evaluator.sortImage(image);
+		Evaluator.printHistogram(test);
 	}
 
 }
